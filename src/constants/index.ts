e6ee0e6dd405fd341951217f984ee1b2728b23c7
tewarig <gauravtewari111@gmail.com>
2024-01-16 22:52:06 +0530

    fix: update icon

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,89 +4,89 @@ export const options = [{
     value: "gauravtewri111@gmail.com"
   },
   {
-    avatar: "http://www.gravatar.com/avatar/1?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_2.png",
     title: "Sample",
     value: "sample@123.com",
   },  
   {
-    avatar: "http://www.gravatar.com/avatar/2?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_3.png",
     title: "Macbook",
     value: "macbook@gmail.com",
   },
   {
-    avatar: "http://www.gravatar.com/avatar/3?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_4.png",
     title: "demo",
     value: "demo3@gmail.com"
   },
   {
-    avatar: "http://www.gravatar.com/avatar/4?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_5.png",
     title: "Cool Stuff",
     value: "coolStuff@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/5?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_6.png",
     title: "not so cool stuff",
     value: "notsocoolstuff@gmail.com",
   },
   {
-    avatar: "http://www.gravatar.com/avatar/6?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_7.png",
     title: "john doe",
     value: "john@gmail.com",
   },
   {
-    avatar: "http://www.gravatar.com/avatar/7?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_10.png",
     title: "Super man",
     value: "Superman@gmail.com",
   },
   {
-    avatar: "http://www.gravatar.com/avatar/8?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_12.png",
     title: "batman",
     value: "batman@gmail.com"
   },
   {
-   avatar: "http://www.gravatar.com/avatar/9?d=identicon",
+   avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_13.png",
     title: "spiderman",
     value: "spiderman@gmail.com" 
   },
   {
-    avatar: "http://www.gravatar.com/avatar/10?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_18.png",
     title: "ironman",
     value: "ironman@gmail.com"
   },
   {
-    avatar: "http://www.gravatar.com/avatar/11?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_27.png",
     title: "hulk",
     value: "hulk@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/12?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_25.png",
     title: "thor",
     value: "thor@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/13?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_18.png",
     title: "captain america",
     value: "captain@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/14?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_1.png",
     title: "black widow",
     value: "blacwindow@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/15?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_13.png",
     title: "hawkeye",
     value: "hawkeye@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/16?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_12.png",
     title: "black panther",
     value: "blacpanther@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/17?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_6.png",
     title: "doctor strange",
     value: "doctorStrange@gmail.com"
   },{
-    avatar: "http://www.gravatar.com/avatar/18?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_5.png",
     title: "wolverine",
     value: "wolverine@gmail.com"
   },
   {
-    avatar: "http://www.gravatar.com/avatar/19?d=identicon",
+    avatar: "https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_3.png",
     title: "deadpool",
     value: "deadpool@gmail.com"
   }
